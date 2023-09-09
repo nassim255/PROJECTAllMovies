2dbe0bfd5fb07e9fac2a3997f47f062ab815c9e2
nassim255 <nassimnassim770@gmail.com>
2023-09-09 02:42:32 +0100

    first commit

--- ProjectAllMovies.js
+++ ProjectAllMovies.js
@@ -54,6 +54,9 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
     items[0].classList.add("carousel__item--selected");
     buttons[0].classList.add("carousel__button--selected");
     });
+
+    /*SLIDER*/
+    
     
     /*timer*/
 
